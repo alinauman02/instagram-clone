a8d3190b978a7b862c1b6f8043a4e6579509ac7b
Ali Nauman <ali.nauman@aurorasolutions.io>
2022-10-12 15:03:30 +0500

    Setup vite plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), eslint(), viteTsconfigPaths(), svgrPlugin()],
+});
