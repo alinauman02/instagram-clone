e71c9d20f627662eeacd3b9261d1b1f21f69be6f
Ejaz hussain <ejazhussain1050@gmail.com>
2022-10-17 16:45:50 +0500

    trying for multiple images

--- frontend/src/components/Post/Content.tsx
+++ frontend/src/components/Post/Content.tsx
@@ -1,5 +1,21 @@
 import './Content.css';
 
-export function Content() {
-  return <div className="content">Content</div>;
+interface Content {
+  type: 'img' | 'video';
+  src: string;
+}
+
+interface ContentProps {
+  content: Content[];
+}
+
+export function Content({ content }: ContentProps) {
+  return;
+
+  {
+    content.map(C => {
+      return C.type == 'img' ? <img src={C.src} alt="eakjf"></img> : <div>not supported</div>;
+    });
+  }
+  <div className="content">Content</div>;
 }
