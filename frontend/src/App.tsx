ba077dc60b17edeea5bd65e9b3241eaf6d343d52
Ejaz hussain <ejazhussain1050@gmail.com>
2022-10-21 16:46:29 +0500

    Rename CreatePostBox state toshowCreatePostModal and handle on Create post click

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 import { Post, Header, Profile, CreatePost } from './components';
+
 function App() {
-  const [CreatePostBox, setCreatePostBox] = useState(true);
+  const [showCreatePostModal, setShowCreatePostModal] = useState(false);
+
   return (
     <div className="app-body">
-      <Header />
-      {CreatePostBox && <CreatePost setCreatePostBox={setCreatePostBox} />}
+      <Header onCreatePostClick={() => setShowCreatePostModal(true)} />
+      {showCreatePostModal && <CreatePost setCreatePostBox={setShowCreatePostModal} />}
       <Post name="Ejaz hussain" number={3} />
       <div className="profile-div">
         <Profile />
