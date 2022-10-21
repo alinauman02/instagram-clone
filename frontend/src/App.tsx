d643bf973c16451cc760d8690be411e8fa98997c
Ejaz hussain <ejazhussain1050@gmail.com>
2022-10-21 10:43:32 +0500

    updates in create post component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import './App.css';
 import { Post, Header, Profile, CreatePost } from './components';
 function App() {
-  const [CreatePostBox, SetCreatePostBox] = useState(true);
+  const [CreatePostBox, setCreatePostBox] = useState(true);
   return (
     <div className="app-body">
       <Header />
-      {CreatePostBox && <CreatePost />}
+      {CreatePostBox && <CreatePost setCreatePostBox={setCreatePostBox} />}
       <Post name="Ejaz hussain" number={3} />
       <div className="profile-div">
         <Profile />
--- frontend/src/components/CreatePost/CreatePost.tsx
+++ frontend/src/components/CreatePost/CreatePost.tsx
@@ -1,9 +1,30 @@
 import './CreatePost.css';
+import { ReactComponent as CreatePostIcon } from 'assets/icons/create-post-icon.svg';
 
-export function CreatePost() {
+interface CreatePostProps {
+  setCreatePostBox: (value: boolean) => void;
+}
+export function CreatePost({ setCreatePostBox }: CreatePostProps) {
   return (
-    <div className="create-post">
-      <div className="create-post-header">Create New Post</div>
+    <div className="create-post-background">
+      <div className="create-post">
+        <div className="create-post-header">Create New Post</div>
+        <div className="create-post-upload">
+          <div className="create-post-icon">
+            <CreatePostIcon />
+          </div>
+
+          <div className="create-post-text">Drag photos and videos here</div>
+          <button
+            className="create-post-button"
+            onClick={() => {
+              setCreatePostBox(false);
+            }}
+          >
+            Select from computer
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
