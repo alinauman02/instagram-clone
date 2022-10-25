3b34027240f96a5fc798a3619f1d618ba1aa345c
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-10-25 11:41:07 +0500

    Remove routerProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { Login, Signup } from './components';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  { path: '/signup', element: <Signup /> },
-  { path: '/home', element: <App /> },
-  { path: '/', element: <Signup /> },
-]);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
