93f755b9f1f2741dd3100c391827733189f631b9
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-10-25 11:41:50 +0500

    Add browserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,23 @@
 import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Post, Header, Profile, CreatePost, Login, Signup } from './components';
-
-
 
 function App() {
   const [showCreatePostModal, setShowCreatePostModal] = useState(false);
 
   return (
     <div className="app-body">
-      <Header onCreatePostClick={() => setShowCreatePostModal(true)} />
+      <BrowserRouter>
+        <Header onCreatePostClick={() => setShowCreatePostModal(true)} />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
       {showCreatePostModal && <CreatePost setCreatePostBox={setShowCreatePostModal} />}
       <Post name="Ejaz hussain" number={3} />
-      <div className="profile-div">
-        <Profile />
-      </div>
-      <Login />
-      <Signup />
     </div>
   );
 }
