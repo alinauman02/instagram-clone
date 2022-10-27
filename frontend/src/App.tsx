33ed0bcccc2130f3be2cc564d02ac4050642d89b
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-10-27 15:32:51 +0500

    Fix formatting issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
-import {  Profile, Login, Signup ,Home} from './components';
+import { Profile, Login, Signup, Home } from './components';
 
 function App() {
   return (
