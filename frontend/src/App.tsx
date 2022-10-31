9274c2117c4225121f6eedf5f651f2b2111c95c8
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-10-31 11:19:26 +0500

    Auto navigate to login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 import './App.css';
 import { Profile, Login, Signup, Home } from './components';
@@ -7,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<Signup />} />
