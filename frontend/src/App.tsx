85358f3714440f5c286c485d4d976453dc981558
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-02 10:23:23 +0500

    Install redux toolkit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { store } from 'store/store';
+import { Provider } from 'react-redux';
 
 import './App.css';
 import { Profile, Login, Signup, Home } from './components';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
