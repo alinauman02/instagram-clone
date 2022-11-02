4eaaa20ccd35fc83ec18c2789a21d9ba8d23070d
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-02 12:39:07 +0500

    create edit password component

---
+++ frontend/src/components/settings/EditPassword.tsx
@@ -0,0 +1,11 @@
+import './EditPassword.css'
+
+export default function EditPassword() {
+  return (
+    <div className='edit-password'>
+        <form>
+            
+        </form>
+    </div>
+  )
+}
