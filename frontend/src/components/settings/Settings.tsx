fe6198ad98d68348fa1c84f0ec2454f86326022f
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-02 13:09:48 +0500

    Add edit modes in settings

--- frontend/src/components/settings/Settings.tsx
+++ frontend/src/components/settings/Settings.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
-import './Settings.css'
-
+import { useState } from 'react';
+import './Settings.css';
 
 export function Settings() {
-    const [changeMode,setChangeMode]=useState<'Password'|'Profile'>('Profile');
-  return (
-    <div className='settings flex-box'>
+  const [selectedEditMode, setSelectedEditMode] = useState<'Password' | 'Profile'>('Profile');
+  let editPasswordSelected = '';
+  let editProfileSelected = '';
+  selectedEditMode == 'Password' ? (editPasswordSelected = 'selected-mode') : (editProfileSelected = 'selected-mode');
 
+  return (
+    <div className="settings flex-box">
+      <div className="settings-sidebar flex-box flex-direction-column">
+        <button
+          className={'edit-buttons ' + editProfileSelected}
+          onClick={() => setSelectedEditMode(mode => 'Profile')}
+        >
+          Edit profile
+        </button>
+        <button
+          className={'edit-buttons ' + editPasswordSelected}
+          onClick={() => setSelectedEditMode(mode => 'Password')}
+        >
+          Change password
+        </button>
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
