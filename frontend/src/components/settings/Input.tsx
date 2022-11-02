7ea9c941806eb3bd9f44a0009e25e23b1cd1bd19
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-02 15:56:38 +0500

    Input component creation

---
+++ frontend/src/components/settings/Input.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Input.css';
+
+interface InputProps {
+  name: string;
+  type: 'string' | 'email' | 'password' | 'number';
+  placeholder: string;
+  onChange: (e: React.FormEvent<HTMLInputElement>) => void;
+}
+
+export function Input({ name, type, placeholder, onChange }: InputProps) {
+  return (
+    <input
+      className="input-edit-field"
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      autoComplete="off"
+    />
+  );
+}
