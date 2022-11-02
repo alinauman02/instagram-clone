2e3a6e80daec499ab37c683d485205481b631344
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-02 11:10:35 +0500

    Provide redux store to app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { store } from 'store';
-import { Provider } from 'react-redux';
 
 import './App.css';
 import { Profile, Login, Signup, Home } from './components';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
