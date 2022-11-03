48cde06035f18e0ca9a781f20f9fb9102c3593e5
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-03 14:58:35 +0500

    Implemented edit phot component

--- frontend/src/components/settings/EditPhoto.tsx
+++ frontend/src/components/settings/EditPhoto.tsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
 import './EditPhoto.css';
 
-export default function EditPhoto() {
-  return <div className="edit-photo">EditPhoto</div>;
+interface EditPhotoProps {
+  profilePic: string;
+  setShowEditPhotoModal: (show: boolean) => void;
+}
+
+export default function EditPhoto({ profilePic, setShowEditPhotoModal }: EditPhotoProps) {
+  const [profilePicture, setProfilePicture] = useState(profilePic);
+  return (
+    <div className="edit-photo-background">
+      <div className="edit-photo flex-box flex-direction-column">
+        {profilePicture === '' ? (
+          <div className="profile-image-icon"></div>
+        ) : (
+          <img className="profile-image-icon" src={profilePicture} alt=""></img>
+        )}
+
+        <button className="upload-photo" onClick={() => setShowEditPhotoModal(false)}>
+          Upload Picture
+        </button>
+        <button className="remove-photo" onClick={() => setProfilePicture('')}>
+          Remove Picture
+        </button>
+      </div>
+    </div>
+  );
 }
