94cc41d1382c68dab36d2501e4a930c7cca408d3
Ali Nauman <ali.nauman@aurorasolutions.io>
2022-11-03 17:38:14 +0500

    Format as a JS file, add tsconfigRootDir

--- backend/functions/.eslintrc.js
+++ backend/functions/.eslintrc.js
@@ -1,26 +1,27 @@
-{
-  "root": true,
-  "env": {
-    "es6": true,
-    "node": true
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  "overrides": [],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "project": ["tsconfig.json", "tsconfig.dev.json"],
-    "sourceType": "module"
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
-  "ignorePatterns": ["/lib/**/*"],
-  "plugins": ["@typescript-eslint", "import"],
-  "rules": {
-    "import/no-unresolved": 0
-  }
-}
+  ignorePatterns: ['/lib/**/*'],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    'import/no-unresolved': 0,
+  },
+};
