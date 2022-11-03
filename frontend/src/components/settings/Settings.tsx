0abeba27447f71107ffbc5658f0ec38ecb4c7a98
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-03 11:58:39 +0500

    Remove extra code

--- frontend/src/components/settings/Settings.tsx
+++ frontend/src/components/settings/Settings.tsx
@@ -1,27 +1,9 @@
-import { useState } from 'react';
+
 import './Settings.css';
-
-import { EditProfile } from './EditProfile';
-import { EditPassword } from './EditPassword';
 export function Settings() {
-  const [selectedEditMode, setSelectedEditMode] = useState<'Password' | 'Profile'>('Profile');
-  let editPasswordSelected = '';
-  let editProfileSelected = '';
-  selectedEditMode == 'Password' ? (editPasswordSelected = 'selected-mode') : (editProfileSelected = 'selected-mode');
-
   return (
-    <div className="settings">
-      <div className="setting-card flex-box">
-        <div className="settings-sidebar flex-box flex-direction-column">
-          <button className={'edit-buttons ' + editProfileSelected} onClick={() => setSelectedEditMode('Profile')}>
-            Edit profile
-          </button>
-          <button className={'edit-buttons ' + editPasswordSelected} onClick={() => setSelectedEditMode('Password')}>
-            Change password
-          </button>
-        </div>
-        <div className="edit-form">{selectedEditMode == 'Password' ? <EditPassword /> : <EditProfile />}</div>
-      </div>{' '}
+    <div>
+      settings
     </div>
   );
 }
