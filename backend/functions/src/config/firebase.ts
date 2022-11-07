9bec6b2c658606428f42e538361515766d1f1532
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-07 18:12:43 +0500

    Import auth and initialize in firebase config

--- backend/functions/src/config/firebase.ts
+++ backend/functions/src/config/firebase.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
 import { getFirestore } from 'firebase-admin/firestore';
 import * as admin from 'firebase-admin';
+import { getAuth } from 'firebase-admin/auth';
 
 admin.initializeApp();
 const firestore = getFirestore();
-
-export { functions, firestore };
+const auth = getAuth(admin.initializeApp());
+export { functions, firestore, auth };
