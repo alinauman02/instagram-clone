fef75fe2ec55f25b01ef330993019751e3bf0404
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-07 18:13:44 +0500

    Add router for user

---
+++ backend/functions/src/resources/user/user.router.ts
@@ -0,0 +1,22 @@
+import { Request, Response, Router } from 'express';
+import { auth } from './../../config';
+import { User } from './user.model';
+
+export async function signUp(user: User) {
+  const res = await auth.createUser({ email: user.email, password: user.password });
+  return res.uid;
+}
+
+const profileRouter = Router();
+
+profileRouter.put('/', async (req: Request, res: Response) => {
+  try {
+    const user: User = req.body.user;
+    const uid = await signUp(user);
+    res.end(uid);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export { profileRouter };
