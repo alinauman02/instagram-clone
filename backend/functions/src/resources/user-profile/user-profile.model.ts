4b03db84c8e4165471c1f392f027303a262d1c8a
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-07 18:14:01 +0500

    Router for user profile

---
+++ backend/functions/src/resources/user-profile/user-profile.model.ts
@@ -0,0 +1,9 @@
+export interface UserProfile {
+  username: string;
+  email: string;
+  phoneNumber: string;
+  gender: 'male' | 'female';
+  name: string;
+  bio: string;
+  uid: string;
+}
---
+++ backend/functions/src/resources/user-profile/user-profile.router.ts
@@ -0,0 +1,35 @@
+import { Request, Response, Router } from 'express';
+import { firestore } from '../../config';
+import { UserProfile } from './user-profile.model';
+
+const profileRouter = Router();
+
+profileRouter.put('/', (req: Request, res: Response) => {
+  const profile: UserProfile = req.body.userProfile;
+  firestore.collection('profiles').doc(profile.uid).set({
+    email: profile.email,
+    username: profile.username,
+    phoneNumber: profile.phoneNumber,
+    bio: profile.bio,
+    name: profile.name,
+    gender: profile.gender,
+    uid: profile.uid,
+  });
+  res.end(profile);
+});
+
+profileRouter.get('/:id', async (req: Request, res: Response) => {
+  if (typeof req.query.id === 'string') {
+    const ref = await firestore.collection('profiles').doc(req.query.id);
+    const doc = await ref.get();
+    if (doc.exists) {
+      res.end(doc);
+    } else {
+      console.log('');
+    }
+  } else {
+    console.log('Error!');
+  }
+});
+
+export { profileRouter };
