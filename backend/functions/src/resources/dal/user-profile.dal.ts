562ab7a889d507d0536e37e28f88030919f7cc29
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-08 17:20:16 +0500

    Add fields createdAt updatedAt etc

--- backend/functions/src/resources/dal/user-profile.dal.ts
+++ backend/functions/src/resources/dal/user-profile.dal.ts
@@ -1,13 +1,15 @@
 import { UserProfile } from '../user-profile';
 import { firestore } from '../../config';
+import { database } from 'firebase-functions/v1/firestore';
 
 const createUserProfileDocument = async (uid: string, profile: UserProfile) => {
-  const ref = firestore.collection('users').doc(uid);
-  return ref.set(profile);
+  const date: Date = new Date();
+  const ref = firestore.collection('user-profiles').doc(uid);
+  return ref.set({ ...profile, createdAt: date, updatedAt: date, isDeleted: false });
 };
 
 const getUserProfileDocument = async (uid: string) => {
-  const ref = firestore.collection('users').doc(uid);
+  const ref = firestore.collection('user-profiles').doc(uid);
   const doc = await ref.get();
   return doc.data();
 };
