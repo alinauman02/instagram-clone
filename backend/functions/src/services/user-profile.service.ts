2c04e93be8d964e4b10dd2ebb3a7a9eeff266445
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-09 13:18:15 +0500

    Create service function for update

--- backend/functions/src/services/user-profile.service.ts
+++ backend/functions/src/services/user-profile.service.ts
@@ -1,7 +1,14 @@
-import { getUserProfileDocument } from '../resources/dal/user-profile.dal';
+import { getUserProfileDocument, patchUserProfileDocument } from '../resources/dal/user-profile.dal';
+import { UserProfile } from '../resources/user-profile';
 
 export async function getUserProfileService(uid: string) {
   const getProfile = await getUserProfileDocument(uid);
+  console.log(getProfile);
+  return getProfile;
+}
+
+export async function patchUserProfileService(uid: string, userProfile: UserProfile) {
+  const getProfile = await patchUserProfileDocument(uid, userProfile);
   console.log(getProfile);
   return getProfile;
 }
