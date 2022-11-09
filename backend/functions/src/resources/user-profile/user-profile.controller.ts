9251dd696bd9c2540a27192c06d5800baeb61235
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-09 13:17:41 +0500

    Controller function for endpoint

--- backend/functions/src/resources/user-profile/user-profile.controller.ts
+++ backend/functions/src/resources/user-profile/user-profile.controller.ts
@@ -1,9 +1,19 @@
 import { RequestHandler } from 'express';
-import { getUserProfileService } from '../../services/user-profile.service';
+import { UserProfile } from '.';
+import { getUserProfileService, patchUserProfileService } from '../../services/user-profile.service';
 
 export const getUserProfile: RequestHandler = async (req, res) => {
   if (typeof req.query.id === 'string') {
     const profile = await getUserProfileService(req.query.id);
+    res.send(profile);
+  } else res.send('Error');
+};
+
+export const patchUserProfile: RequestHandler = async (req, res) => {
+  if (typeof req.query.id === 'string') {
+    const userProfile: UserProfile = req.body.profile;
+    console.log(req.query.id, '   ', userProfile);
+    const profile = await patchUserProfileService(req.query.id, userProfile);
     res.send(profile);
   } else res.send('Error');
 };
