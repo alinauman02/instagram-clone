d8e30da8aabaf770412f0dbaac939adedfb10d09
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-09 13:17:59 +0500

    Create route for update

--- backend/functions/src/resources/user-profile/user-profile.router.ts
+++ backend/functions/src/resources/user-profile/user-profile.router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { getUserProfile } from './user-profile.controller';
+import { getUserProfile,patchUserProfile } from './user-profile.controller';
 
 export const profileRouter = Router();
 
 profileRouter.get('/', getUserProfile);
+profileRouter.patch('/', patchUserProfile);
