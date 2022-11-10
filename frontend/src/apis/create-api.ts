a29813272a9ca6e6809b2418fa45038586d430e8
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-10 18:07:55 +0500

    Create Api call for using RTK query

--- frontend/src/apis/create-api.ts
+++ frontend/src/apis/create-api.ts
@@ -1,14 +1,27 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { UserProfile } from 'models';
+export interface Post {
+  id: string;
+  profile: UserProfile;
+}
 
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:3002/aurora-instagram-clone/us-central1/api/profile/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:3002/aurora-instagram-clone/us-central1/api' }),
   endpoints: builder => ({
     getProfileById: builder.query<UserProfile, string>({
-      query: id => `${id}`,
+      query: id => `/profile/${id}`,
+    }),
+    updateProfileById: builder.mutation<UserProfile, Post>({
+      query: ({ id, profile }) => ({
+        url: `/profile/${id}`,
+        method: 'PATCH',
+        body: {
+          profile,
+        },
+      }),
     }),
   }),
 });
 
-export const { useGetProfileByIdQuery } = api;
+export const { useGetProfileByIdQuery, useUpdateProfileByIdMutation } = api;
