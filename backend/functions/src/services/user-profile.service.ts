17a70ec7d8d9b5469958b02623f2d38fd75bdb13
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-10 18:06:40 +0500

    Remove extra console

--- backend/functions/src/services/user-profile.service.ts
+++ backend/functions/src/services/user-profile.service.ts
@@ -3,12 +3,10 @@ import { UserProfile } from '../resources/user-profile';
 
 export async function getUserProfileService(uid: string) {
   const getProfile = await getUserProfileDocument(uid);
-  console.log(getProfile);
   return getProfile;
 }
 
 export async function patchUserProfileService(uid: string, userProfile: UserProfile) {
   const getProfile = await patchUserProfileDocument(uid, userProfile);
-  console.log(getProfile);
   return getProfile;
 }
