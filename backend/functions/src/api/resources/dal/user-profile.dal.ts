de2988b3710b278de7f037467e854355d293cecc
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-14 12:54:52 +0500

    Use helping function to update object

--- backend/functions/src/api/resources/dal/user-profile.dal.ts
+++ backend/functions/src/api/resources/dal/user-profile.dal.ts
@@ -1,21 +1,11 @@
 import { UserProfile } from '../user-profile';
 import { firestore } from '../../../config';
 import { COLLECTION_NAMES } from '../../../constants';
+import { onCreateDocument, onUpdateDocument } from '../../../utils';
 
-export const updateDocumentDate = (ref: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>) => {
-  const date: Date = new Date();
-  ref.set({ updatedAt: date }, { merge: true });
-};
-
-export const createDocumentDate = (ref: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>) => {
-  const date: Date = new Date();
-  ref.set({ updatedAt: date, createdAt: date, isDeleted: false }, { merge: true });
-};
-
-export const createUserProfileDocument = async (uid: string, profile: UserProfile) => {
-  const date: Date = new Date();
+export const createUserProfileDocument = async (uid: string, userProfile: UserProfile) => {
   const ref = firestore.collection(COLLECTION_NAMES.USER_PROFILES).doc(uid);
-  ref.set({ ...profile, createdAt: date, updatedAt: date, isDeleted: false });
+  ref.set({ ...onCreateDocument(userProfile) });
   return await (await ref.get()).data();
 };
 
@@ -27,7 +17,6 @@ export const getUserProfileDocument = async (uid: string) => {
 
 export const updateUserProfileDocument = async (uid: string, userProfile: UserProfile) => {
   const ref = firestore.collection(COLLECTION_NAMES.USER_PROFILES).doc(uid);
-  ref.update({ ...userProfile });
-  updateDocumentDate(ref);
+  ref.update({ ...onUpdateDocument(userProfile) });
   return await (await ref.get()).data();
 };
