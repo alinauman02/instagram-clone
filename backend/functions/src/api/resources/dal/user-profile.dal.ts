88d9db5f97e8bd8455a434991476fb18b2d3ef69
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-14 11:56:09 +0500

    Use enum for collection names

--- backend/functions/src/api/resources/dal/user-profile.dal.ts
+++ backend/functions/src/api/resources/dal/user-profile.dal.ts
@@ -1,23 +1,32 @@
 import { UserProfile } from '../user-profile';
-import { firestore } from '../../config';
+import { firestore } from '../../../config';
 
-const createUserProfileDocument = async (uid: string, profile: UserProfile) => {
+export const updateDocumentDate = (ref: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>) => {
   const date: Date = new Date();
-  const ref = firestore.collection('user-profiles').doc(uid);
-  return ref.set({ ...profile, createdAt: date, updatedAt: date, isDeleted: false });
+  ref.set({ updatedAt: date }, { merge: true });
 };
 
-const getUserProfileDocument = async (uid: string) => {
-  const ref = firestore.collection('user-profiles').doc(uid);
+export const createDocumentDate = (ref: FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>) => {
+  const date: Date = new Date();
+  ref.set({ updatedAt: date, createdAt: date, isDeleted: false }, { merge: true });
+};
+
+export const createUserProfileDocument = async (uid: string, profile: UserProfile) => {
+  const date: Date = new Date();
+  const ref = firestore.collection(COLLECTION_NAMES.USER_PROFILES).doc(uid);
+  ref.set({ ...profile, createdAt: date, updatedAt: date, isDeleted: false });
+  return await (await ref.get()).data();
+};
+
+export const getUserProfileDocument = async (uid: string) => {
+  const ref = firestore.collection(COLLECTION_NAMES.USER_PROFILES).doc(uid);
   const doc = await ref.get();
   return doc.data();
 };
 
-const patchUserProfileDocument = async (uid: string, userProfile: UserProfile) => {
-  const ref = firestore.collection('user-profiles').doc(uid);
-  const date: Date = new Date();
-  const doc = ref.update({ ...userProfile, updatedAt: date });
-  return doc;
+export const updateUserProfileDocument = async (uid: string, userProfile: UserProfile) => {
+  const ref = firestore.collection(COLLECTION_NAMES.USER_PROFILES).doc(uid);
+  ref.update({ ...userProfile });
+  updateDocumentDate(ref);
+  return await (await ref.get()).data();
 };
-
-export { createUserProfileDocument, getUserProfileDocument, patchUserProfileDocument };
