bb0061bda74d27b29ae74ff2ccdc436891043a87
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-15 15:09:08 +0500

    Automate refetching on updates

--- frontend/src/apis/create-api.ts
+++ frontend/src/apis/create-api.ts
@@ -8,9 +8,11 @@ export interface Post {
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:3002/aurora-instagram-clone/us-central1/api' }),
+  tagTypes: ['Post'],
   endpoints: builder => ({
     getProfileById: builder.query<UserProfile, string>({
       query: id => `/user-profiles/${id}`,
+      providesTags: ['Post'],
     }),
     updateProfileById: builder.mutation<UserProfile, Post>({
       query: ({ id, profile }) => ({
@@ -18,6 +20,7 @@ export const api = createApi({
         method: 'PATCH',
         body: profile,
       }),
+      invalidatesTags: ['Post'],
     }),
   }),
 });
