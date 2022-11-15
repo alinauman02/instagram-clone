60aeb2ea404235ea7d37dc298dbc010a52da3b79
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-15 15:07:14 +0500

    Error handling in sign up function

--- backend/functions/src/api/resources/auth/auth.controller.ts
+++ backend/functions/src/api/resources/auth/auth.controller.ts
@@ -1,15 +1,19 @@
 import { RequestHandler } from 'express';
 import { signUpUser } from '.';
-import { createUserProfileDocument , UserProfile } from '../user-profile';
+import { createUserProfileDocument, UserProfile } from '../user-profile';
 
-export const signup: RequestHandler = async (req, res) => {
-  const name: string = req.body.name;
-  const email: string = req.body.email;
-  const password: string = req.body.password;
-  const username: string = req.body.username;
-  const profile: UserProfile = { name, username, email, phoneNumber: '', gender: '', bio: '' };
+export const signup: RequestHandler = async (req, res, next) => {
+  try {
+    const name: string = req.body.name;
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+    const username: string = req.body.username;
+    const profile: UserProfile = { name, username, email, phoneNumber: '', gender: '', bio: '' };
 
-  const userRecord = await signUpUser(email, password);
-  await createUserProfileDocument(userRecord.uid, profile);
-  res.send({ ...profile, id: userRecord.uid });
+    const userRecord = await signUpUser(email, password);
+    await createUserProfileDocument(userRecord.uid, profile);
+    res.send({ ...profile, id: userRecord.uid });
+  } catch (error) {
+    next(error);
+  }
 };
