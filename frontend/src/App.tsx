8f5544d57d376daf3abde1405870e7c15cbf66dd
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-16 16:28:54 +0500

    Store user id and token in auth slice reducer state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
 
 import './App.css';
-import { Profile, Login, Signup, Home, Settings, EditProfile, EditPassword } from './components';
+import { Profile, Login, Signup, Home, Settings, EditProfile, EditPassword } from 'components';
+import { auth } from 'config';
+import { setAuthState, useAppDispatch } from 'store';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async user => {
+      if (!user) return;
+
+      const id = user.uid;
+      const token = await user.getIdToken(true);
+      dispatch(setAuthState({ id, token }));
+    });
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <div className="app-body">
         <Routes>
           <Route path="/" element={<Navigate to="login" />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
           <Route path="/settings" element={<Settings />}>
             <Route path="/settings" element={<Navigate to="edit-profile" />} />
