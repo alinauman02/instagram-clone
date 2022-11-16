5c1fa6c33e9a3f33b69aa15dc8161a1cfc0b7d03
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-16 12:28:44 +0500

    Send response from create user document

--- backend/functions/src/api/resources/auth/auth.controller.ts
+++ backend/functions/src/api/resources/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from 'express';
 import { signUpUser } from '.';
-import { createUserProfileDocument , UserProfile } from '../user-profile';
+import { createUserProfileDocument, UserProfile } from '../user-profile';
 
 export const signup: RequestHandler = async (req, res) => {
   const name: string = req.body.name;
@@ -8,8 +8,6 @@ export const signup: RequestHandler = async (req, res) => {
   const password: string = req.body.password;
   const username: string = req.body.username;
   const profile: UserProfile = { name, username, email, phoneNumber: '', gender: '', bio: '' };
-
   const userRecord = await signUpUser(email, password);
-  await createUserProfileDocument(userRecord.uid, profile);
-  res.send({ ...profile, id: userRecord.uid });
+  res.send(await createUserProfileDocument(userRecord.uid, profile));
 };
