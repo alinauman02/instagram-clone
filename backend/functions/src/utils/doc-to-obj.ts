fe4c6531c09da77496b2a53f2b4a242ee769fb63
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-16 16:58:37 +0500

    Remove field createdAt

--- backend/functions/src/utils/doc-to-obj.ts
+++ backend/functions/src/utils/doc-to-obj.ts
@@ -1,5 +1,6 @@
 export function docToObj(snapShot: FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData>) {
   const data = snapShot.data();
   const id = snapShot.id;
-  return { ...data, id, updatedAt: data?.updatedAt.toDate(), createdAt: data?.createdAt.toDate() };
+  delete data?.createdAt;
+  return { ...data, id, updatedAt: data?.updatedAt.toDate() };
 }
