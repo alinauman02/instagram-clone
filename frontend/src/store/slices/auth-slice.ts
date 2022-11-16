397b19e1d44c3f7e151f5f66538fe667e648cde8
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-16 15:41:24 +0500

    Create auth slice with token and id as states

---
+++ frontend/src/store/slices/auth-slice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from 'store/store';
+
+interface AuthState {
+  id: string;
+  token: string;
+}
+
+const initialState: AuthState = { id: '', token: '' };
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setAuthState(state, action: PayloadAction<AuthState>) {
+      state.id = action.payload.id;
+      state.token = action.payload.token;
+    },
+  },
+});
+
+export const selectUserId = (state: RootState) => state.auth.id;
+export const selectUserToken = (state: RootState) => state.auth.token;
+
+export const { setAuthState } = authSlice.actions;
+export const authSliceReducer = authSlice.reducer;
