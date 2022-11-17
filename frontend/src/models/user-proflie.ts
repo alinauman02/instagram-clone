7f28b444479b2c37fa85f88fdef31200de0aefac
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-17 11:34:25 +0500

    Extend metadata interface for reusable fields

--- frontend/src/models/user-proflie.ts
+++ frontend/src/models/user-proflie.ts
@@ -1,11 +1,10 @@
-export interface UserProfile {
+import { Metadata } from './meta-data';
+
+export interface UserProfile extends Metadata {
   username: string;
   email: string;
   phoneNumber: string;
   gender: 'male' | 'female' | '';
   name: string;
   bio: string;
-  createdAt?: Date;
-  updatedAt?: Date;
-  isDeleted?: boolean;
 }
