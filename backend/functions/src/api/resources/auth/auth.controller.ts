26e61183a23652e47eecf06d04af8039d28633d3
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-22 13:41:08 +0500

    Validate auth

--- backend/functions/src/api/resources/auth/auth.controller.ts
+++ backend/functions/src/api/resources/auth/auth.controller.ts
@@ -1,15 +1,14 @@
+import { plainToClass } from 'class-transformer';
+import { validateOrReject } from 'class-validator';
 import { RequestHandler } from 'express';
 import { signUpUser } from '.';
 import { createUserProfileDocument, UserProfile } from '../user-profile';
 
 export const signup: RequestHandler = async (req, res, next) => {
   try {
-    const name: string = req.body.name;
-    const email: string = req.body.email;
-    const password: string = req.body.password;
-    const username: string = req.body.username;
-    const profile: UserProfile = { name, username, email, phoneNumber: '', gender: '', bio: '' };
-    const userRecord = await signUpUser(email, password);
+    const profile: UserProfile = plainToClass(UserProfile, { ...req.body, phoneNumber: '', gender: '', bio: '' });
+    await validateOrReject(profile);
+    const userRecord = await signUpUser(req.body.email, req.body.password);
     res.send(await createUserProfileDocument(userRecord.uid, profile));
   } catch (error) {
     next(error);
