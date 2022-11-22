4811cc544f1315de7506255d35b24f9b8c9c6d55
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-22 13:41:30 +0500

    Added validation in user profile

--- backend/functions/src/api/resources/user-profile/user-profile.controller.ts
+++ backend/functions/src/api/resources/user-profile/user-profile.controller.ts
@@ -1,20 +1,23 @@
+import { plainToClass } from 'class-transformer';
+import { validateOrReject } from 'class-validator';
 import { RequestHandler } from 'express';
 import { UserProfile } from '.';
 import { getUserProfileService, updateUserProfileService } from './user-profile.service';
 
-export const getUserProfile: RequestHandler = async (req, res,next) => {
+export const getUserProfile: RequestHandler = async (req, res, next) => {
   try {
     const profile = await getUserProfileService(req.params.id);
-  res.send(profile);
+    await validateOrReject(UserProfile);
+    res.send(profile);
   } catch (error) {
     next(error);
   }
-  
 };
 
 export const UpdateUserProfile: RequestHandler = async (req, res, next) => {
   try {
-    const userProfile: UserProfile = req.body;
+    const userProfile = plainToClass(UserProfile, req.body);
+    await validateOrReject(userProfile);
     const profile = await updateUserProfileService(req.params.id, userProfile);
     res.send(profile);
   } catch (error) {
