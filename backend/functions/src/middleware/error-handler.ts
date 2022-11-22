391db24e4f55c508ddb13a3f6716f8ce196b04c7
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-22 13:42:45 +0500

    Adjust error handling for validations

--- backend/functions/src/middleware/error-handler.ts
+++ backend/functions/src/middleware/error-handler.ts
@@ -1,9 +1,15 @@
-import { Request, Response, NextFunction } from 'express';
+import { isArray } from 'class-validator';
+import { NextFunction, Request, Response } from 'express';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
-  res.status(500).json({
-    error: error.message,
-  });
+  if (isArray(error)) {
+    const temp = error[0].constraints;
+    console.log(temp);
+    res.status(500).json(temp);
+  } else
+    res.status(500).json({
+      error: error?.message,
+    });
 };
