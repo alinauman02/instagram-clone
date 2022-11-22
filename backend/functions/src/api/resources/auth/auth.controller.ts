fdd8fbb2f2e0b68942b9da6477e7ec75d10689bb
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-22 17:06:01 +0500

    Use payload class in auth

--- backend/functions/src/api/resources/auth/auth.controller.ts
+++ backend/functions/src/api/resources/auth/auth.controller.ts
@@ -1,15 +1,25 @@
-import { plainToClass } from 'class-transformer';
 import { validateOrReject } from 'class-validator';
 import { RequestHandler } from 'express';
 import { signUpUser } from '.';
 import { createUserProfileDocument, UserProfile } from '../user-profile';
+import { SignupRequestPayLoad } from './auth.model';
 
 export const signup: RequestHandler = async (req, res, next) => {
   try {
-    const profile: UserProfile = plainToClass(UserProfile, { ...req.body, phoneNumber: '', gender: '', bio: '' });
+    const profile = new SignupRequestPayLoad(req.body.username, req.body.email, req.body.password, req.body.name);
     await validateOrReject(profile);
     const userRecord = await signUpUser(req.body.email, req.body.password);
-    res.send(await createUserProfileDocument(userRecord.uid, profile));
+    const userProfile = new UserProfile(
+      profile.username,
+      profile.email,
+      profile.name,
+      req.body.bio,
+      req.body.phoneNumber,
+      req.body.createdAt,
+      req.body.updatedAt,
+      req.body.isDeleted
+    );
+    res.send(await createUserProfileDocument(userRecord.uid, userProfile));
   } catch (error) {
     next(error);
   }
