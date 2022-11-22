53a14993f4e84d96c2f32cda004b780c61d34a43
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-22 13:40:46 +0500

    Add validation class

--- backend/functions/src/api/resources/user-profile/user-profile.model.ts
+++ backend/functions/src/api/resources/user-profile/user-profile.model.ts
@@ -1,10 +1,27 @@
-export interface UserProfile {
-  username: string;
-  email: string;
+import { IsAlpha, IsEmail, IsNumberString, Length, MaxLength, MinLength } from 'class-validator';
+
+export class UserProfile {
+  @MinLength(10, {
+    message: 'Username is too short',
+  })
+  @MaxLength(20, {
+    message: 'Username is too long',
+  })
+  username?: string;
+
+  @IsEmail({},{ message: 'Invalid email' })
+  email?: string;
+
+  @Length(10, 13)
+  @IsNumberString()
   phoneNumber?: string;
+
   gender?: 'male' | 'female' | '';
+
+  @IsAlpha()
   name?: string;
   bio?: string;
+
   createdAt?: Date;
   updatedAt?: Date;
   isDeleted?: boolean;
