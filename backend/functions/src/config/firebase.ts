298a93b5cc86e0635e9ef8dad0e2073c2751c5a2
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-23 16:47:09 +0500

    Ignore undefined properties in firestore

--- backend/functions/src/config/firebase.ts
+++ backend/functions/src/config/firebase.ts
@@ -1,9 +1,10 @@
-import * as functions from 'firebase-functions';
-import { getFirestore } from 'firebase-admin/firestore';
 import * as admin from 'firebase-admin';
 import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
+import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 const firestore = getFirestore();
+firestore.settings({ ignoreUndefinedProperties: true });
 const auth = getAuth();
 export { functions, firestore, auth };
