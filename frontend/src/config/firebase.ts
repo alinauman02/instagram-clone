57d81f60cb5747a103306544eefab94a06571175
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-23 11:19:24 +0500

    Use variable for url

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
@@ -11,9 +11,10 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_APP_FIREBASE_MID,
 };
 
+const url = 'http://localhost:3001';
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-connectAuthEmulator(auth, 'http://localhost:3001');
+connectAuthEmulator(auth, url);
 
 export { auth, app };
