f13d6e86852aface703a150ee671d2359974e3f2
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-24 18:46:20 +0500

    Optimize sending data

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -1,16 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { UserProfile } from 'models';
+import { RootState } from 'store';
 
 export const urlString = 'http://127.0.0.1:3002/aurora-instagram-clone/us-central1/api/';
 
 export interface PostProfile {
   id: string;
-  token: string;
   profile: UserProfile;
 }
 
 // Define a service using a base URL and expected endpoints
 export const emptyApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: urlString }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: urlString,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+
   endpoints: () => ({}),
 });
