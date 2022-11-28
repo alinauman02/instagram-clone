8ee8390a2a920b70fabeea7ae5ec2148ea396e37
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-28 17:45:41 +0500

    Correct spelling

--- backend/functions/src/middleware/can-edit-profile.ts
+++ backend/functions/src/middleware/can-edit-profile.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export const canEditProfile = (req: Request, res: Response, next: NextFunction) => {
   if (req.body.user.uid === req.params.id) {
-    console.log('verified AUthorization');
+    console.log('verified Authorization');
     next();
   } else next(new Error("'Authorization Error'"));
 };
