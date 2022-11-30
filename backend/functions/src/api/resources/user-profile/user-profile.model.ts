1a0078574080152e00502a1ca36839439aa9d592
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-30 11:59:53 +0500

    Deleted extra variables

--- backend/functions/src/api/resources/user-profile/user-profile.model.ts
+++ backend/functions/src/api/resources/user-profile/user-profile.model.ts
@@ -29,28 +29,11 @@ export class UserProfile {
   @Length(0, 150)
   bio?: string;
 
-  createdAt?: Date;
-  updatedAt?: Date;
-  isDeleted?: boolean;
-
-  constructor(
-    username: string,
-    email: string,
-    name: string,
-    bio?: string,
-    phoneNumber?: string,
-    gender?: Gender,
-    createdAt?: Date,
-    updatedAt?: Date,
-    isDeleted?: boolean
-  ) {
+  constructor(username: string, email: string, name: string, bio?: string, phoneNumber?: string, gender?: Gender) {
     this.username = username;
     this.email = email;
     this.name = name;
     this.bio = bio ? bio : '';
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.isDeleted = isDeleted;
     this.phoneNumber = phoneNumber;
     this.gender = gender;
   }
