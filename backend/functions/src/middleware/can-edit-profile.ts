86598986587165acdf77704c49e2f0e2d88bdcdd
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-11-30 11:34:38 +0500

    Make message clear

--- backend/functions/src/middleware/can-edit-profile.ts
+++ backend/functions/src/middleware/can-edit-profile.ts
@@ -4,7 +4,7 @@ export const canEditProfile = (req: Request, res: Response, next: NextFunction) 
   try {
     if (req.user.uid === req.params.id) {
       next();
-    } else throw new Error("'Authorization Error'");
+    } else throw new Error('Authorization Error! User is not allowed');
   } catch (error) {
     next(error);
   }
