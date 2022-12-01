88455c794f793ab28b7ddfdd50c16d1a9eaf16d9
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-01 17:08:50 +0500

    Adjust routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,18 +26,22 @@ function App() {
     <BrowserRouter>
       <div className="app-body">
         <Routes>
-          <Route path="/" element={<Navigate to="login" />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/:username" element={<Profile />} />
-          {useAppSelector(selectUserId) && (
+          {useAppSelector(selectUserId) ? (
             <>
               <Route path="/home" element={<Home />} />
+              <Route path="/:username" element={<Profile />} />
               <Route path="/settings" element={<Settings />}>
                 <Route path="/settings" element={<Navigate to="edit-profile" />} />
                 <Route path="/settings/change-password" element={<EditPassword />} />
                 <Route path="/settings/edit-profile" element={<EditProfile />} />
               </Route>
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Navigate to="login" />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/:username" element={<Profile />} />
             </>
           )}
         </Routes>
