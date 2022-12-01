4d96216df41a0b46a567198c2ec4bab633bb6c07
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-01 16:43:57 +0500

    Req user can be undefined

--- backend/functions/src/api/resources/middleware/can-edit-profile.ts
+++ backend/functions/src/api/resources/middleware/can-edit-profile.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export const canEditProfile = (req: Request, res: Response, next: NextFunction) => {
   try {
-    if (req.user.uid === req.params.id) {
+    if (req.user?.uid === req.params.id) {
       next();
     } else throw new Error('Authorization Error! User is not allowed');
   } catch (error) {
