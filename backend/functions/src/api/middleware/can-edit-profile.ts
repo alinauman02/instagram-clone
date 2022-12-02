f96dc07dec4efa7d4e36446bee280307214c794e
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-02 13:09:37 +0500

    Remove redundant try catch

--- backend/functions/src/api/middleware/can-edit-profile.ts
+++ backend/functions/src/api/middleware/can-edit-profile.ts
@@ -1,11 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const canEditProfile = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    if (req.user?.uid === req.params.id) {
-      next();
-    } else throw new Error('Authorization Error! User is not allowed');
-  } catch (error) {
-    next(error);
-  }
+  if (req.user?.uid === req.params.id) {
+    next();
+  } else next(new Error('Authorization Error! User is not allowed'));
 };
--- backend/functions/src/api/middleware/validate-id-token.ts
+++ backend/functions/src/api/middleware/validate-id-token.ts
@@ -2,13 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { getAuth } from 'firebase-admin/auth';
 
 export const validateIdToken = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    if (req.headers.authorization) {
-      const decodedToken = await getAuth().verifyIdToken(req.headers.authorization.split(' ')[1]);
-      req.user = decodedToken;
-      next();
-    } else throw new Error('Authentication error! Token not found');
-  } catch (error) {
-    next(error);
-  }
+  if (req.headers.authorization) {
+    const decodedToken = await getAuth().verifyIdToken(req.headers.authorization.split(' ')[1]);
+    req.user = decodedToken;
+    next();
+  } else next(new Error('Authentication error! Token not found'));
 };
