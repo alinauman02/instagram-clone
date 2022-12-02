09fc252f518d9f1982921fb3941d21419caf2232
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-02 12:58:48 +0500

    Use validateIdToken middleware after get request

--- backend/functions/src/api/resources/user-profile/user-profile.router.ts
+++ backend/functions/src/api/resources/user-profile/user-profile.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { canEditProfile } from '../../middleware';
+import { canEditProfile, validateIdToken } from '../../middleware';
 import { getUserProfile, getUserProfileByUserName, updateUserProfile } from './user-profile.controller';
 
 export const profileRouter = Router();
 
 profileRouter.get('/:id', getUserProfile);
 profileRouter.get('/username/:username', getUserProfileByUserName);
+profileRouter.use(validateIdToken);
 profileRouter.patch('/:id', canEditProfile, updateUserProfile);
