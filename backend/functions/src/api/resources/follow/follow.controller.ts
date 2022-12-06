77421928069cfb50a90117adae13723abe56ebe9
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-06 17:58:59 +0500

    create controller for follow router

---
+++ backend/functions/src/api/resources/follow/follow.controller.ts
@@ -0,0 +1,49 @@
+import { plainToInstance } from 'class-transformer';
+import { validateOrReject } from 'class-validator';
+import { RequestHandler } from 'express';
+import { FollowRequestPlayLoad } from './follow.model';
+import {
+  followUserProfileService,
+  getFOllowedByUserProfilesService,
+  getFollowingUserProfilesService,
+  unFollowUserProfileService,
+} from './follow.service';
+
+export const getFOllowedByUserProfiles: RequestHandler = async (req, res, next) => {
+  try {
+    const profile = await getFOllowedByUserProfilesService(req.params.username);
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getFollowingUserProfiles: RequestHandler = async (req, res, next) => {
+  try {
+    const profile = await getFollowingUserProfilesService(req.params.username);
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const followUserProfile: RequestHandler = async (req, res, next) => {
+  try {
+    const userProfile = plainToInstance(FollowRequestPlayLoad, req.body as FollowRequestPlayLoad);
+    await validateOrReject(userProfile);
+    const profile = await followUserProfileService(req.params.id, userProfile);
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const unFollowUserProfile: RequestHandler = async (req, res, next) => {
+  try {
+    const uid = req.user?.uid ?? '';
+    const profile = await unFollowUserProfileService(req.params.id, uid);
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+};
