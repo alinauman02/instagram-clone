cbec2ccce451870f054353ab9646f7c0b22ef91e
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-07 10:58:13 +0500

    Create Profile List component for displaying component

---
+++ frontend/src/components/ui/ProfileList/ProfileList.tsx
@@ -0,0 +1,12 @@
+import './ProfileList.css';
+
+export function ProfileList() {
+  return (
+    <div className="profile-list-background">
+      <div className="profile-list">
+        <div className="profile-list-header">Followers</div>
+        <div>EJAZ HUSSAIN</div>
+      </div>
+    </div>
+  );
+}
