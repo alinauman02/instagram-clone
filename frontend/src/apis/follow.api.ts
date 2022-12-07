f6fec275de201356b0b21fec2067473b35cfbddf
Ejaz Hussain <ejaz.hussain@aurorasolutions.io>
2022-12-07 18:54:11 +0500

    Correct mistakes in follow api

--- frontend/src/apis/follow.api.ts
+++ frontend/src/apis/follow.api.ts
@@ -1,5 +1,5 @@
 import { UserProfile } from 'models';
-import { emptyApi, followAccount } from '.';
+import { emptyApi } from '.';
 
 export enum ApiTag {
   PROFILE = 'Profile',
@@ -9,24 +9,23 @@ const apiWithTag = emptyApi.enhanceEndpoints({ addTagTypes: [ApiTag.PROFILE] });
 const followersAPi = apiWithTag.injectEndpoints({
   endpoints: builder => ({
     getFollowers: builder.query<UserProfile, string>({
-      query: username => `/follower/followed-by-user-profiles/${username}`,
+      query: username => `/follow/followed-by-user-profiles/${username}`,
       providesTags: [ApiTag.PROFILE],
     }),
     getFollowings: builder.query<UserProfile, string>({
-      query: username => `/follower/following-user-profiles/${username}`,
+      query: username => `/follow/following-user-profiles/${username}`,
       providesTags: [ApiTag.PROFILE],
     }),
-    followUserProfile: builder.mutation<UserProfile, followAccount>({
-      query: ({ id, uid, name, username }) => ({
-        url: `/follower/follow-user-profile${id}`,
+    followUserProfile: builder.mutation<UserProfile, string>({
+      query: username => ({
+        url: `/follow/follow-user-profile/${username}`,
         method: 'PATCH',
-        body: { uid, name, username },
       }),
       invalidatesTags: [ApiTag.PROFILE],
     }),
     unFollowUserProfile: builder.mutation<UserProfile, string>({
-      query: uid => ({
-        url: `/follower/unfollow-user-profile${uid}`,
+      query: username => ({
+        url: `/follow/unfollow-user-profile/${username}`,
         method: 'PATCH',
       }),
       invalidatesTags: [ApiTag.PROFILE],
